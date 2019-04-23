eee745c4430c55aa6162d751d968a7cb329a65ce
Sinkyears <1669487366@qq.com>
2019-04-23 17:11:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ww8mpn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ww8mpn)
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Start1</h1>`;
